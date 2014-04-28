e11633228eb3eb5c5a06ce99be20d532a39fecb7
ptrapani <paul.t.trapani@gmail.com>
2014-04-28 00:34:38 -0400

    Add reset button and win logic
    Adding reset button.  Added horizontal and vertical win logic

--- TieTacToe/TicTacToe.js
+++ TieTacToe/TicTacToe.js
@@ -1,71 +1,95 @@
- var playerTurn = 1;
- var playerSymbol = "X";
- var IsGameOver = "false";
- $("#display-message").text("Player " + playerTurn + "'s Turn");
+//TO DO: makes these init values their own function.  Might need to make variables global though or namespace or properties of the methods
+
+var playerTurn = 1;
+var playerSymbol = "X";
+var IsGameOver = "false";
+$("#display-message").text("Player " + playerTurn + "'s Turn");
+
+$(".square").click(function () {
+    if ($(this).text() === "") {
+        if (playerTurn === 1) {
+            $(this).text(playerSymbol);
+            CheckWinner();
+
+        } else {
+            $(this).text(playerSymbol);
+            CheckWinner();
+        }
+    } else {
+        $("#display-message").text("Not a valid move");
+    }
+});
+
+$("#reset").on("click", function () {
+    RestartGame();
+});
+
+function SwitchPlayer() {
+    //Switch turns
+    if (playerTurn === 1) {
+        playerTurn = 2;
+        playerSymbol = "O";
+    } else {
+        playerTurn = 1;
+        playerSymbol = "X";
+    }
+
+    $("#display-message").text("Player " + playerTurn + "'s Turn");
+}
 
 
- $(".square").click(function () {
+function CheckWinner() {
+    var isGameOver = "false";
+    var NoWinner = "true";
+    var winningCombo = playerSymbol += playerSymbol += playerSymbol;
 
-     if ($(this).text() === "") {
-         if (playerTurn === 1) {
-             $(this).text(playerSymbol);
-             CheckWinner();
+    //Traverse through board to determine if 3 in a row
+    //Only check for current player
 
-         } else {
-             $(this).text(playerSymbol);
-             CheckWinner();
-         }
-     } else {
-         $("#display-message").text("Not a valid move");
-     }
- });
+    //Check horizontal
+    if (winningCombo === $("#row1 .square").text() || winningCombo === $("#row2 .square").text() || winningCombo === $("#row3 .square").text()) {
+        isGameOver = "true";
+        NoWinner = "false";
+    }
 
 
- function Main() {
-     //Switch turns
-     if (playerTurn === 1) {
-         playerTurn = 2;
-         playerSymbol = "O";
-     } else {
-         playerTurn = 1;
-         playerSymbol = "X";
-     }
+    //Check vertical. 
+    if (winningCombo === $(".col1").text() || winningCombo === $(".col2").text() || winningCombo === $(".col3").text()) {
+        isGameOver = "true";
+        NoWinner = "false";
+    }
 
-     $("#display-message").text("Player " + playerTurn + "'s Turn");
- }
+    //Check Diagonal
+
+    DetermineNextTurn(isGameOver, NoWinner);
+}
 
 
- function CheckWinner() {
-     var isGameOver = "false";
-     var NoWinner = "true";
+function DetermineNextTurn(isGameOver, NoWinner) {
+    if (isGameOver === "true") {
 
-     //Traverse through board to determine if 3 in a row
-     //Only check for current player
+        //Board is full, no winner
+        if (NoWinner === "true") {
+            $("#display-message").text("It's a draw!");
+            $("#reset").show();
+            //TO DO: Make board inactive         
+        }
+        //A player has won
+        else {
+            $("#display-message").text("Player " + playerTurn + " has won!");
+            $("#reset").show();
+            //TO DO: Make board inactive
+        }
+    }
+    //Continue to next turn
+    else {
+        SwitchPlayer();
+    }
+}
 
-
-     DetermineNextTurn(isGameOver, NoWinner);
- }
-
-
- function DetermineNextTurn(isGameOver, NoWinner) {
-     if (isGameOver === "true") {
-
-         //Board is full, no winner
-         if (NoWinner === "true") {
-             $("#display-message").text("It's a draw!");
-             //Reset button displays            
-         }
-         //A player has won
-         else {
-             $("#display-message").text("Player " + playerTurn + " has won!");
-         }
-     }
-     //Continue to next turn
-     else {
-         Main();
-     }
- }
-
- function RestartGame() {
-     //Clear board and make it player 1's turn   
- }
+function RestartGame() {
+    //Clear board and make it player 1's turn
+    //Might not even need this actually
+    //TO DO: Empty out all the squares so that reset works   
+    //InitNewGame();
+}
