01d03a791dfdcd280d4e98ea2c5055852a764468
ptrapani <paul.t.trapani@gmail.com>
2014-04-27 21:00:43 -0400

    Adding some functionality
    Added click event

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,1 +1,20 @@
-This is a test
+$(document).ready(function () {
+    var playerTurn = 1;
+
+    $(".square").click(function () {
+
+        if ($(this).text() === "") {
+            if (playerTurn === 1) {
+                $(this).text("X");
+            } else {
+                $(this).text("O");
+            }
+        }
+    });
+    
+    
+    function CheckWinner(){
+    }
+    
+    
+});
